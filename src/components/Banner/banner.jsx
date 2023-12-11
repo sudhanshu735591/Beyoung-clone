24070ab7a8987da186d3907949fd19a7b0519683
Sudhanshu <ersshekhar05@gmail.com>
2023-12-11 12:52:30 +0530

    updated code push

--- src/components/Banner/banner.jsx
+++ src/components/Banner/banner.jsx
@@ -5,8 +5,8 @@ import "./Banner.css";
 function Banner(){
     return(
             <div className="imageBanner"> 
-                <img className="img" src="https://www.beyoung.in/api/catalog/homepage-3-10/banner-new/black-friday-banner-desktop-view.jpg"/>
-                <img className="img" src="https://beyoung.in/api/catalog/homepage-5-dec/desktop/desktop-free-shipping11.png"/>
+                <img className="img" src="https://www.beyoung.in/api/catalog/homepage-28-11/main-banner/SHIRT-BANNER-DESKTOP-VIEW.jpg"/>
+                <img className="img" style={{marginTop:"20px"}} src="https://beyoung.in/api/catalog/homepage-5-dec/desktop/desktop-free-shipping11.png"/>
 
             </div>
     )
