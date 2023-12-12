c5a58a4eba1d48c1cccf1436e0b56dfcf367fd86
Sudhanshu <ersshekhar05@gmail.com>
2023-12-12 21:23:49 +0530

    updated code push

--- src/components/App.js
+++ src/components/App.js
@@ -14,7 +14,6 @@ function App() {
     <div className="App">
       <Routes>
         <Route path = "/" element = {<Home/>}/>
-        {/* <Route path = "/categories" element = {<Categories/>}/> */}
         <Route path = "/imageDetails/:id" element = {<ImageDetails/>}/>
         <Route path = "/categorized" element = {<CategorizedSection/>}/>
 
--- src/components/GenderSection/gendersection.jsx
+++ src/components/GenderSection/gendersection.jsx
@@ -1,12 +1,26 @@
 import { Link, json, useSearchParams } from "react-router-dom";
 import "./gendersection.css";
 import Tshirts_Categories from "../MenTshirtCategories/menTshirtsCategories";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import BottomWear from "../MensBottomWear/mensBottomWear";
 import WomenSection from "../WomenSection/womenSection";
 
 function GenderSection(props) {
   let {category, gender, shirts, demand } = props;
+
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  const containerref = useRef();
+
+  const handleScroll = (scrollAmount)=>{
+    const newScrollPosition = scrollPosition + scrollAmount;
+    setScrollPosition(newScrollPosition);
+    containerref.current.scrollLeft = newScrollPosition;
+    
+  }
+
+
+
 
   return (
     <div className="parentDiv">
@@ -22,31 +36,50 @@ function GenderSection(props) {
       </div>
 
       <div className="imageData">
-        <div>
-          <Link>
-            <img style={{border:"none"}} src={Tshirts_Categories.t_Shirts.img}/>
-          </Link>
+        <div className="flex"  >
+          <div className="flex1">
+            <Link>
+              <div 
+              className="nextArrow">
+                <i class="fa-solid fa-angle-down"></i>
+              </div>
+              <img style={{border:"none"}} src={Tshirts_Categories.t_Shirts.img}/>
+            </Link>
+          </div>
+          
+          <div className="flex2" ref={containerref}
+            style={{
+              width:"900px",
+              overflowX:"scroll",
+              scrollBehavior:"smooth",
+            }}
+            >
+            {
+              category && category.t_Shirts.list.map((val)=>{
+                return(
+                  <Link to={(`/categorized?search=${JSON.stringify(val.search)}&filter=${JSON.stringify(val.filter)}`)}>
+                    <div className="flex3">
+                      
+                      <img  className="sideImageData" src = {val.img}/>
+                      <p className="tshirtText">{val.name}</p>
+                    </div>
+                  </Link>
+                )
+              })
+            }
+          </div>
         </div>
-        <div className="imageCategories">
-          {
-            category && category.t_Shirts.list.map((val)=>{
-              return(
-                <Link to={(`/categorized?search=${JSON.stringify(val.search)}&filter=${JSON.stringify(val.filter)}`)}>
-                  <div>
-                    <img  className="sideImageData" src = {val.img}/>
-                    <p className="tshirtText">{val.name}</p>
-                  </div>
-                </Link>
-              )
-            })
-          }
+        <div 
+       
+        className="rightArrow">
+          <i  class="fa-solid fa-angle-down"></i>
         </div>
       </div>
 
       <div className="displayImage">
         <div className="demand">
           <div className="tshirt1">BOTTOMWEAR</div>
-          <div className="highondemand">{demand}</div>
+          <div className="highondemand high1">{demand}</div>
         </div>
         <div className="viewall">View All</div>
       </div>
@@ -57,10 +90,10 @@ function GenderSection(props) {
           BottomWear.bottom.map((val)=>{
             return(
               <Link to={(`/categorized?search=${JSON.stringify(val.search)}&filter=${JSON.stringify(val.filter)}`)}>
-              <div>
-                <img className="bottomWear" src = {val.img}/>
-                <p className="bottoWearText">{val.name}</p>
-              </div>
+                <div>
+                  <img className="bottomWear" src = {val.img}/>
+                  <p className="bottoWearText">{val.name}</p>
+                </div>
               </Link>
             )
           })
