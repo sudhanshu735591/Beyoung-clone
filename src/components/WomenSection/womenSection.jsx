8b6e5c50d0248296a308620b49d0509bd81e3841
Sudhanshu <ersshekhar05@gmail.com>
2023-12-23 13:19:02 +0530

    updated code push

--- src/components/WomenSection/womenSection.jsx
+++ src/components/WomenSection/womenSection.jsx
@@ -1,97 +1,46 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./womenSection.css";
-import { NavLink } from "react-router-dom";
-
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-
-function WomenSection(props){
-    let {gender} = props;
-
-    let [womenData, setWoemnData] = useState([]);
-
-    let Women = "Women";
 
 
-    const fetchAPi = async ()=>{
-        let apiUrl = `https://academics.newtonschool.co/api/v1/ecommerce/clothes/products?filter={"gender":"Women"}`
-        let resData = await fetch(apiUrl , {
-            method: 'GET',
-            headers : {
-              projectID: "zx5u429ht9oj",
-            }
-        });
-        let res = await resData.json();
-        setWoemnData(res);
-    }
+function WomenSection(props) {
+  let { gender } = props;
+  let [womenData, setWomenData] = useState([]);
 
-    useEffect(()=>{
-        fetchAPi();
-    }, [])
+  const fetchAPI = async () => {
+    let apiUrl = `https://academics.newtonschool.co/api/v1/ecommerce/clothes/products?filter={"gender":"Women"}`;
+    let resData = await fetch(apiUrl, {
+      method: "GET",
+      headers: {
+        projectID: "zx5u429ht9oj",
+      },
+    });
+    let res = await resData.json();
+    setWomenData(res);
+  };
 
+  useEffect(() => {
+    fetchAPI();
+  }, []);
 
+  
+  return (
+    <div>
+      <div className="mensection">{gender}</div>
+      <div className="imageSlider">
 
-    const SampleNextArrow = (props) => {
-        const { className, style, onClick } = props;
-        return (
-          <div
-            className={className}
-            style={{ ...style, display: "block", background: "red" }}
-            onClick={onClick}
-            
-          />
-        );
-      }
-    
-      const SamplePrevArrow = (props) => {
-        const { className, style, onClick } = props;
-        return (
-          <div
-            className={className}
-            style={{ ...style, display: "block", background: "green" }}
-            onClick={onClick}
-          />
-        );
-      }
-
-
-    const settings = {
-        speed: 500,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 3000,
-        cssEase: "linear",
-        cssEase: "linear",
-        nextArrow: <SampleNextArrow />,
-        prevArrow: <SamplePrevArrow />
-    };
-    
-
-    return(
-        <div>
-            <div className="mensection">{gender}</div>
-            <div>
-               
-                <div className="slider">
-                <Slider {...settings}>
-                    {
-                         womenData.data && womenData.data.map((val)=>{
-                            return(
-                                <div className="card">
-                                    <img className="womenImage" src={val.displayImage} alt="Image"/>
-                                </div>
-                            )
-                         })
-                    }
-                </Slider>
+        <div className="slider">
+          <div className="womenSectionFlex">
+          {womenData.data &&
+              womenData.data.map((val, index) => (
+                <div className="card" key={index}>
+                  <img className="womenImage" src={val.displayImage} alt="Image" />
                 </div>
-                
-            </div>
-
+              ))}
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default WomenSection;
