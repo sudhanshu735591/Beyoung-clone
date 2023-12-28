f66a24bce1a6ae96149301a647c3b5e5b16c0193
Sudhanshu <ersshekhar05@gmail.com>
2023-12-28 08:34:27 +0530

    cart data

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -6,6 +6,7 @@ import SearchBar from "../SearchBar/searchBar";
 import UserContext from "../../ContextApi/UserContext";
 import { createPortal } from "react-dom";
 import SignUpPage from "../../Auth/Signup/Signup";
+import styled from "@emotion/styled";
 
 function Navbar(){
 
@@ -14,6 +15,8 @@ function Navbar(){
     const {successMessage} = useContext(UserContext);
 
     // const {cartCount} = useContext(UserContext);
+    const {token} = useContext(UserContext);
+
 
     function onHandleClick(){
       setSearch(search?false:true);
@@ -133,9 +136,9 @@ function Navbar(){
 
                 <div className="addToWishList">
                     {
-                        <div className="addtonumWishList">
+                        <div className="addtonumWishList" style={{background:!token && "white"}}>
                             {
-                                localStorage.getItem("WishListData") && localStorage.getItem("WishListData")!==null ? JSON.parse(localStorage.getItem("WishListData")).length: null
+                                localStorage.getItem("WishListData") && token && localStorage.getItem("WishListData")!==null ? JSON.parse(localStorage.getItem("WishListData")).length: null
                             } 
                         </div>
                     }
@@ -144,8 +147,8 @@ function Navbar(){
                 <svg onClick={handleWishList} width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M22 8.06253C22 15.0874 12.0004 21 12.0004 21C12.0004 21 2 15 2 8.07677C2 5.25003 4.22222 3.00003 7 3.00003C9.77778 3.00003 12 6.37503 12 6.37503C12 6.37503 14.2222 3.00003 17 3.00003C19.7778 3.00003 22 5.25003 22 8.06253Z" stroke="black" stroke-width="1.5" stroke-linecap="round"></path></svg>
 
                 <div className="addToCart">
-                    {<div className="addToNumber">{
-                        localStorage.getItem("data")!==null ? JSON.parse(localStorage.getItem("data")).length: <div></div>
+                    {<div className="addToNumber" style={{background:!token && "white"}}>{
+                        localStorage.getItem("data")!==null && token ? JSON.parse(localStorage.getItem("data")).length: <div></div>
                     }
                 </div>}
 
