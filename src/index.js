f348233d03b12cbbeb0dc7b920de35aa7425ee64
Sudhanshu <ersshekhar05@gmail.com>
2024-01-12 01:00:23 +0530

    updated code push

--- src/index.js
+++ src/index.js
@@ -7,12 +7,22 @@ import "slick-carousel/slick/slick.css";
 
 import "slick-carousel/slick/slick-theme.css";
 import UserContextProvider from "./ContextApi/UserContextProvider";
-
+import { Offline, Online } from "react-detect-offline";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+    <>
+    <Online>
     <BrowserRouter>
     <UserContextProvider>
         <App />
     </UserContextProvider>
     </BrowserRouter>
+    </Online>
+
+    <Offline>
+        <div className="offline">
+            <h1>Please make sure your system is connected to the internet !!!</h1>
+        </div>
+    </Offline>
+    </>
 );
